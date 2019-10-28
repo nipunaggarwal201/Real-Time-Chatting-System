81f073ed376c4eb43b105390bad085b6b9d3de67
Nipun Aggarwal <nipunaggarwal201@gmail.com>
2019-10-28 11:51:14 +0530

    first

---
+++ index.php
@@ -0,0 +1,74 @@
+<?php
+	include 'db.php';
+?>
+
+<!DOCTYPE html>
+<html lang = 'en'>
+<head>
+	<meta charset='UTF-8'>
+	<title>Chat Appliaction</title>
+	
+	<link rel = "stylesheet" href = "style.css" media ="all">
+	
+	<script>
+		function ajax(){
+			
+			var req = new XMLHttpRequest();
+			
+			req.onreadystatechange = function(){
+				
+				if(req.readyState == 4 && req.status == 200){
+					
+					document.getElementById('chat').innerHTML = req.responseText;
+					
+				}
+			}
+				req.open('POST','chat.php',true);
+				req.send();
+		}
+		
+		setInterval(function(){ajax()},1000);
+	
+	</script>
+</head>
+<body onload = "ajax();">
+	<div id = "container">
+	
+			<div id ="chat_box">
+			<div id = "chat"></div>
+			
+			</div>
+			<form method = "post" action = "index.php">
+			<input type = "text" name ="name" placeholder = "Enter name"/>
+			<textarea name ="msg" placeholder = "Enter message"></textarea>
+			<input type = "submit" name ="submit" value = "Send"/>
+			
+			</form>
+			
+			<?php
+			if(isset($_POST['submit'])){
+				
+				
+				$name = $_POST['name'];
+				$msg = $_POST['msg'];
+				
+				
+				
+				$query = "INSERT INTO chat1 (name,msg) values ('$name','$msg')";
+				
+				$run = $con->query($query);
+				
+				if($run){
+					echo "<embed loop='false' src='chat.wav' hidden='true' autoplay='true'/>";
+					
+				}
+				
+			}
+						
+			
+			?>
+			
+	</div>
+
+</body>
+</html>
